c5860ce5d4d38a5cd7df250dfb4f8ebbc27bbf6a
Ethan Tumy <ethan@Ethans-MacBook-Pro.local>
2018-06-25 10:52:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# webserverw
+# webserverw
---
+++ websever.go
@@ -0,0 +1,28 @@
+package main
+
+import ("fmt"
+  "net/http")
+
+func main() {
+
+    
+
+    http.HandleFunc("/earth", handler2)
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    		http.ServeFile(w, r, r.URL.Path[1:])
+    	})
+
+
+    http.ListenAndServe(":8080", nil)
+
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+      fmt.Fprintf(w, "Hello World") }
+
+func handler2(w http.ResponseWriter, r *http.Request){
+            fmt.Fprintf(w, "Hello earth")
+
+}
